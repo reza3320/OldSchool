055823a2bc68a9299417f00f0aa090d4aa641d03
reza3320 <reza3320@gmail.com>
2024-10-12 15:33:06 -0700

    Added Firebase Authentication setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
 }
